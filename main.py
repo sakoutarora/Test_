f00e5f08bc1fd2ba44472cd4499dd40b5cfc6311
Sakoutarora <sakshamarora090@gmail.com>
2022-04-16 14:16:56 +0530

    "changes in main"

--- main.py
+++ main.py
@@ -12,4 +12,6 @@ chain_id = 5777
 my_address = '0xb77Ca4F10A6d28dbCfb87B9fbfA5A1b99328D67F'
 private_key = '0xf0c972ca1d2bfa22e4d0e08967c507cf458d3017f8b34260f4b2e54b6aa54198'
 simple_store = w3.eth.contract(abi=abi, bytecode=bytecodes)
-print(w3.eth.get_block('latest'))
+# print(w3.eth.get_block('latest'))
+print(w3.eth.get_balance('0x19E35915cdf5D5583472760fae2aF71F98673fA6'))    # 100000000000000000000 or 100 eth 
+print(w3.fromWei(w3.eth.get_balance('0x19E35915cdf5D5583472760fae2aF71F98673fA6'), 'ether'))
